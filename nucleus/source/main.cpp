61cff6efc099c715d7df249cccf37d7af01707ef
CAxl <88182623+CAxl@users.noreply.github.com>
2025-10-17 12:03:51 +0200

    return after long time, print all 16O SPE in main (no other changes)

--- nucleus/source/main.cpp
+++ nucleus/source/main.cpp
@@ -36,7 +36,10 @@ int main() {
 	for (int i = 0; i < r.size(); ++i) std::cout << r[i] << std::endl;
 	
 
-	// proton orbital: 0s_1/2
+
+	/* Tabulating single-particle energies for proton orbitals in 16O */
+
+	// proton orbital 16O: s_1/2
 	{
 		int l = 0;
 		double j = 0.5;
@@ -44,53 +47,130 @@ int main() {
 		std::cout << "Proton eigenvalues (O16, 0s1/2):\n" << eigvals << "\n";
 	}
 
+	// proton orbital 16O: p_3/2
+	{
+		int l = 1;
+		double j = 1.5;
+		Eigen::VectorXd eigvals = solve_mean_field(A, Z, l, j, r, dx, true, 5);
+		std::cout << "Proton eigenvalues (O16, 0p_3/2):\n" << eigvals << "\n";
+	}
 
-	// proton 0s1/2
+	// proton orbital 16O: p_1/2
+	{
+		int l = 1;
+		double j = 0.5;
+		Eigen::VectorXd eigvals = solve_mean_field(A, Z, l, j, r, dx, true, 5);
+		std::cout << "Proton eigenvalues (O16, 0p_1/2):\n" << eigvals << "\n";
+	}
+
+	// proton orbital 16O: d_5/2
+	{
+		int l = 2;
+		double j = 2.5;
+		Eigen::VectorXd eigvals = solve_mean_field(A, Z, l, j, r, dx, true, 5);
+		std::cout << "Proton eigenvalues (O16, 0d_5/2):\n" << eigvals << "\n";
+	}
+
+	// proton orbital 16O: d_3/2
+	{
+		int l = 2;
+		double j = 1.5;
+		Eigen::VectorXd eigvals = solve_mean_field(A, Z, l, j, r, dx, true, 5);
+		std::cout << "Proton eigenvalues (O16, 0d_3/2):\n" << eigvals << "\n";
+	}
+
+
+	/* Tablulating single-particle energies for neutron orbitals in 16O */
+	
+	// neutron orbital 16O: s_1/2
 	{
 		int l = 0;
 		double j = 0.5;
-		auto wfns = solve_mean_field_wavefuncs(A, Z, l, j, r, dx, true, 5, false);// last true flag is for R = u/r
-
-		Eigen::MatrixXd u_norm = normalize_wavefunction(r, wfns[0]);
-
-		for (int i = 0; i < r.size(); ++i) {
-			std::cout << wfns[0][i] << std::endl;
-		}
-
-		/* 
-		note 7/9: 
-		- norm of R not correct! \int r^2 R(r)\: dr, whereas \int u(r)\: dr
-		- If I actually write the u_norm() to xy-file and plot (u_norm)^2 in python I get 1 (FOR u(r) ONLY)
-		
-		
-		*/ 
-		write_xy_to_file(r, u_norm, "../../../../Output/p_norm_test.txt");
-		//write_xy_to_file(r, wfns[0], "../../../../Output/p_Rwf_s1_2_1.txt");
-		//write_xy_to_file(r, u_norm, "../../../../Output/p_NormRwf_s1_2_1.txt");
+		Eigen::VectorXd eigvals = solve_mean_field(A, Z, l, j, r, dx, false, 5);
+		std::cout << "Neutron eigenvalues (O16, 0s1/2):\n" << eigvals << "\n";
 	}
 
-
-
-
-	/* BE test */
-
-
-	double BE = BindingEnergy(A, Z)[0];
-	double BE_per_A = BindingEnergy(A, Z)[1];
-	std::cout << "Binding energy 16O = " << BE << std::endl;
-	std::cout << "Binding energy per nucleon = " << BE_per_A << std::endl;
-
-
-
-
-	/* (x,v(r)) data */
-	std::vector<double> V_tot(r.size());  
-	std::vector<double> VC = V_C(r, A, Z);
-	std::vector<double> VWS = V_WS(r, A, Z);
-
-	for (size_t i = 0; i < r.size(); ++i) {
-		V_tot[i] = VC[i] + VWS[i];
+	// neutron orbital 16O: p_3/2
+	{
+		int l = 1;
+		double j = 1.5;
+		Eigen::VectorXd eigvals = solve_mean_field(A, Z, l, j, r, dx, false, 5);
+		std::cout << "Neutron eigenvalues (O16, 0p_3/2):\n" << eigvals << "\n";
 	}
+
+	// neutron orbital 16O: p_1/2
+	{
+		int l = 1;
+		double j = 0.5;
+		Eigen::VectorXd eigvals = solve_mean_field(A, Z, l, j, r, dx, false, 5);
+		std::cout << "Neutron eigenvalues (O16, 0p_1/2):\n" << eigvals << "\n";
+	}
+
+	// neutron orbital 16O: d_5/2
+	{
+		int l = 2;
+		double j = 2.5;
+		Eigen::VectorXd eigvals = solve_mean_field(A, Z, l, j, r, dx, false, 5);
+		std::cout << "Neutron eigenvalues (O16, 0d_5/2):\n" << eigvals << "\n";
+	}
+
+	// neutron orbital 16O: d_3/2
+	{
+		int l = 2;
+		double j = 1.5;
+		Eigen::VectorXd eigvals = solve_mean_field(A, Z, l, j, r, dx, false, 5);
+		std::cout << "Neutron eigenvalues (O16, 0d_3/2):\n" << eigvals << "\n";
+	}
+
+	/*--------------------------------------------------------------------------------------*/
+
+
+	//// proton 0s1/2 wavefuncs
+	//{
+	//	int l = 0;
+	//	double j = 0.5;
+	//	auto wfns = solve_mean_field_wavefuncs(A, Z, l, j, r, dx, true, 5, false);// last true flag is for R = u/r
+
+	//	Eigen::MatrixXd u_norm = normalize_wavefunction(r, wfns[0]);
+
+	//	for (int i = 0; i < r.size(); ++i) {
+	//		std::cout << wfns[0][i] << std::endl;
+	//	}
+
+	//	/* 
+	//	note 7/9: 
+	//	- norm of R not correct! \int r^2 R(r)\: dr, whereas \int u(r)\: dr
+	//	- If I actually write the u_norm() to xy-file and plot (u_norm)^2 in python I get 1 (FOR u(r) ONLY)
+	//	
+	//	
+	//	*/ 
+	//	write_xy_to_file(r, u_norm, "../../../../Output/p_norm_test.txt");
+	//	//write_xy_to_file(r, wfns[0], "../../../../Output/p_Rwf_s1_2_1.txt");
+	//	//write_xy_to_file(r, u_norm, "../../../../Output/p_NormRwf_s1_2_1.txt");
+	//}
+
+
+
+
+	///* BE test */
+
+
+	//double BE = BindingEnergy(A, Z)[0];
+	//double BE_per_A = BindingEnergy(A, Z)[1];
+	//std::cout << "Binding energy 16O = " << BE << std::endl;
+	//std::cout << "Binding energy per nucleon = " << BE_per_A << std::endl;
+
+ //// ----------------------------------------------------------------------------
+
+
+	///* (x,v(r)) data */
+	//std::vector<double> V_tot(r.size());  
+	//std::vector<double> VC = V_C(r, A, Z);
+	//std::vector<double> VWS = V_WS(r, A, Z);
+
+	//for (size_t i = 0; i < r.size(); ++i) {
+	//	V_tot[i] = VC[i] + VWS[i];
+	//}
 
 	//write_xy_to_file(r, V_tot, "../../../../Output/V_WS+V_C");
 	//write_xy_to_file(r, VC, "../../../../Output/V_C");
